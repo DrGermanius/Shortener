8cadd8d01562c091b8a46ebc752b61d36d2a4373
hermansamolazov <samolazov.herman@gmail.com>
2021-12-26 20:07:53 +0300

    code increment #12

--- internal/app/models/batch.go
+++ internal/app/models/batch.go
@@ -1,11 +1,11 @@
 package models
 
 type BatchOriginal struct {
-	CorrelationId string `json:"correlation_id"`
+	CorrelationID string `json:"correlation_id"`
 	OriginalURL   string `json:"original_url"`
 }
 
 type BatchShort struct {
-	CorrelationId string `json:"correlation_id"`
+	CorrelationID string `json:"correlation_id"`
 	ShortURL      string `json:"short_url"`
 }
